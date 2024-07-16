dad10c478b3dc72040c35f7f5588c2039a5db2af
Don Kirkham <dkirkham@dminc.com>
2024-07-16 06:22:06 -0500

    List Items working

--- codeSegment/Script.cs
+++ codeSegment/Script.cs
@@ -11,8 +11,19 @@ public class Script : ScriptBase
         if (Context.OperationId == "Query")
         {
             string sessionId = await GetSessionId().ConfigureAwait(false);
-            //Context.Logger.LogInformation($"sessionId: {sessionId}");
             return await HandleQueryOperation(sessionId).ConfigureAwait(false);
+        }
+
+        if (Context.OperationId == "ListItemsAtPath")
+        {
+            string sessionId = await GetSessionId().ConfigureAwait(false);
+            return await HandleListItemsOperation(sessionId).ConfigureAwait(false);
+        }
+
+        if (Context.OperationId == "DownloadItemContent")
+        {
+            string sessionId = await GetSessionId().ConfigureAwait(false);
+            return await HandleDownloadItemContentOperation(sessionId).ConfigureAwait(false);
         }
 
         // Handle an invalid operation ID
@@ -23,11 +34,15 @@ public class Script : ScriptBase
 
     private async Task<string> GetSessionId()
     {
-        //Context.Logger.LogInformation($"Getting sessionId");
+        Context.Logger.LogInformation($"Getting sessionId");
         try
         {
-            string username = "harmony.integration@sb-takeda.com";
-            string password = "Mediv@2030";
+            //dev
+            //string username = "harmony.integration@sb-takeda.com";
+            //string password = "Mediv@2030";
+            //prod
+            string username = "harmony.integration@takeda.com";
+            string password = "Mediv@2024";
             string requestUrl = $"https://{Context.Request.RequestUri?.Host}/api/v23.1/auth";
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
             request.Headers.TryAddWithoutValidation("Accept", "application/json");
@@ -58,9 +73,108 @@ public class Script : ScriptBase
             var contentJson = JsonConvert.DeserializeObject<JObject>(contentString);
             string query = contentJson?["q"]?.ToString() ?? string.Empty;
             Context.Logger.LogInformation($"query: {query}");
-            var body = new List<KeyValuePair<string, string>>{new("q", query)};
+            var body = new List<KeyValuePair<string, string>> { new("q", query) };
             Context.Request.Content = new FormUrlEncodedContent(body);
 
+            HttpResponseMessage response = await Context.SendAsync(Context.Request, CancellationToken);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var responseJson = JObject.Parse(responseString);
+
+            var data = responseJson["data"];
+            var next = responseJson["responseDetails"]?["next_page"]?.ToString() ?? "";
+            while (next != "")
+            {
+                Context.Logger.LogInformation($"next: {next}");
+                var requestUri = new Uri($"https://{Context.Request.RequestUri?.Host}{next}");
+                var request = new HttpRequestMessage(Context.Request.Method, requestUri);
+                request.Headers.TryAddWithoutValidation("Authorization", sessionId);
+                response = await Context.SendAsync(request, CancellationToken);
+                response.EnsureSuccessStatusCode();
+                responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                responseJson = JObject.Parse(responseString);
+
+                var newdata = responseJson["data"] ?? Enumerable.Empty<JToken>();
+                data = JToken.FromObject(data?.Concat(newdata) ?? Enumerable.Empty<JToken>());
+                next = responseJson["responseDetails"]?["next_page"]?.ToString() ?? "";
+            }
+            responseJson["data"] = data;
+            //Context.Logger.LogInformation($"data: {JsonConvert.SerializeObject(data)}");
+            response.Content = CreateJsonContent(responseJson.ToString());
+            return response;
+        }
+        catch (Exception ex)
+        {
+            return new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Content = new StringContent(ex.ToString())
+            };
+        }
+    }
+
+    private async Task<HttpResponseMessage> HandleListItemsOperation(string sessionId)
+    {
+        Context.Logger.LogInformation($"Executing List Items Operation");
+        try
+        {
+            Context.Request.Headers.TryAddWithoutValidation("Authorization", sessionId);
+#pragma warning disable CS8604 // Possible null reference argument.
+            Context.Request.RequestUri = new UriBuilder(Context.Request.RequestUri)
+            {
+                Path = Uri.UnescapeDataString(Context.Request.RequestUri.AbsolutePath).Replace("//", "/")
+            }.Uri;
+#pragma warning restore CS8604 // Possible null reference argument.
+            HttpResponseMessage response = await Context.SendAsync(Context.Request, CancellationToken);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var responseJson = JObject.Parse(responseString);
+
+            var data = responseJson["data"];
+            var next = responseJson["responseDetails"]?["next_page"]?.ToString() ?? "";
+            while (next != "")
+            {
+                Context.Logger.LogInformation($"next: {next}");
+                var requestUri = new Uri($"https://{Context.Request.RequestUri?.Host}{next}");
+                var request = new HttpRequestMessage(Context.Request.Method, requestUri);
+                request.Headers.TryAddWithoutValidation("Authorization", sessionId);
+                response = await Context.SendAsync(request, CancellationToken);
+                response.EnsureSuccessStatusCode();
+                responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                responseJson = JObject.Parse(responseString);
+
+                var newdata = responseJson["data"] ?? Enumerable.Empty<JToken>();
+                data = JToken.FromObject(data?.Concat(newdata) ?? Enumerable.Empty<JToken>());
+                next = responseJson["responseDetails"]?["next_page"]?.ToString() ?? "";
+            }
+            responseJson["data"] = data;
+            //Context.Logger.LogInformation($"data: {JsonConvert.SerializeObject(data)}");
+            response.Content = CreateJsonContent(responseJson.ToString());
+            return response;
+        }
+        catch (Exception ex)
+        {
+            return new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Content = new StringContent(ex.ToString())
+            };
+        }
+    }
+
+    private async Task<HttpResponseMessage> HandleDownloadItemContentOperation(string sessionId)
+    {
+        Context.Logger.LogInformation($"Executing Download Item Content Operation");
+        try
+        {
+            Context.Request.Headers.TryAddWithoutValidation("Authorization", sessionId);
+#pragma warning disable CS8604 // Possible null reference argument.
+            Context.Request.RequestUri = new UriBuilder(Context.Request.RequestUri)
+            {
+                Path = Uri.UnescapeDataString(Context.Request.RequestUri.AbsolutePath)
+                    .Replace("//", "/")
+                    .Replace("%3A", ":")
+            }.Uri;
             HttpResponseMessage response = await Context.SendAsync(Context.Request, CancellationToken);
             response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
